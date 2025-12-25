f97045a440d40f736bf52efb33165830e3f6d2cd
Nenad Ljubik <nenad.ljubik@echoios.com>
2025-12-25 16:34:57 +0100

    Initial commit

---
+++ MovieTime/MovieTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTimeApp.swift
+//  MovieTime
+//
+//  Created by Ненад Љубиќ on 25.12.25.
+//
+
+import SwiftUI
+
+@main
+struct MovieTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
